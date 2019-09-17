931de5528795938e9996f9cd37ce2217267ed7c1
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-17 08:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(!!cardNumber){
-    var total = total();
+    var totalCost = total();
     cart = []
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }else{
     return "Sorry, we don't have a credit card on file for you."
   }
