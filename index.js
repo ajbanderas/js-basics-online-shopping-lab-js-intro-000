a4a6db9957a5a6b82eded218aa294830e3a4e7d9
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-17 08:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,5 +62,8 @@ function placeOrder(cardNumber) {
   if(!!cardNumber){
     var total = total()
     cart = []
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }else{
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
